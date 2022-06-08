eeea536f6061466dd47d0fface55247d4d5187e9
Chris Kaltwasser <chris@chrisk.com>
2022-06-08 12:00:33 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # robby
+
+This is a rust version of a GA program covered in 
+the Book "Complexity - A Guided Tour", by Melanie Mitchell
+
+The goal is to start with a simple implmenetation of GA
+and then then look for ways that RUST abstractions can
+be used to effectively extend the code base to use a simple
+GP approach.
+
---
+++ src/params.rs
@@ -0,0 +1,62 @@
+//***************************************************************************/
+//*                                                                         */
+//*                           PARAMS                                        */
+//*                                                                         */
+//*                    Parameter file for GA                                */
+//*                                                                         */
+//***************************************************************************/
+
+enum SelectionMethod {
+    FitnessProportionate, 
+    LinearRank, 
+    PureRank,
+    SigmaScaling,
+    Elite,
+}
+
+pub struct RunParams { 
+    // Experiment parameters
+    num_runs:   i64,
+
+    // GA parameters
+    num_generations:    i64,
+
+    // Options for selection_method are "fitness proportionate", "linear rank", "pure rank"
+    // "sigma scaling", and "elite"
+    selection_method:   String,
+    pop_size:           i64,
+    chrom_length:       i64,
+    mutation_rate:      f64, // probability of mutation at each locus in a chromosome
+    crossover_sites:    i64,
+    crossover_rate:     f64, // probability of two parents crossing over
+    rank_max_weight:    f64, // Maximum weight for rank scaling 
+    sigma_scaling_max_weight: f64, // Maximum weight for sigma scaling 
+    num_elite:          f64, // fill in if selection method is "elite" 
+}
+
+pub struct IOParams { 
+    run_num_dir: String,
+    output_dir: String,
+    snapshot_interval: i64,
+    short_print:    bool,
+    long_print:    bool,
+    best_print:    bool,
+}
+
+pub struct FitnessParams { 
+    fitness_function_name: String,
+    wall_penalty: i64, // Points lost for crashing into a wall
+    can_reward: i64, // Points gained for picking up a can
+    can_penalty: i64, // Points lost for trying to pick up a can in an empty cell 
+    num_moves: i64, // Number of moves a robot makes per session 
+    num_environments_for_fitness: i64, // Number of environments each 
+				  // individual is tested on for 
+				  //# calculating fitness
+}
+
+pub struct Params {
+    pub run: RunParams,
+    pub io: IOParams,
+    pub fitness: FitnessParams,
+}
+
